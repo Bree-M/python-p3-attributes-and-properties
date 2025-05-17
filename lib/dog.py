9c1dc0b970f3dd3d6a40f9bd8d7e4d6640c0d4bc
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-05-17 04:04:49 +0300

    still on objects

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+class Dog:
+
+    APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -11,5 +13,30 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-class Dog:
-    pass
+    def __init__(self, name="Mimi", breed="Corgi"):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return getattr(self, "_name", None)
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def breed(self):
+        return getattr(self, "_breed", None)
+
+    @breed.setter
+    def breed(self, value):
+        if value in Dog.APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+class Person:
+    
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -15,5 +17,33 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
-class Person:
-    pass
+
+    def __init__(self,name="Wilson",job="Legal"):
+        self.name = name
+        self.job = job
+     
+    @property 
+    def name(self):
+        return getattr (self, "_name", None) 
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+    
+    @property
+    def job(self):
+        return getattr (self, "_job", None)
+
+    @job.setter            
+    def job(self,value):
+        if value in Person.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")  
+            self._job = None  
+
+
